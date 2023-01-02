ed23c84390af32e301f06972884dc8a8aad90f57
Xaznu <krawczynskikewin@gmail.com>
2023-01-02 14:25:17 +0100

    Added "Category" class to Models file

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -2,6 +2,10 @@
 {
     public class Category
     {
+       
         public int MyProperty { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
 }
