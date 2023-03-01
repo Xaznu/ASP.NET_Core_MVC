6340130701f21afff0dd854729f25fa47ad219cc
Xaznu <krawczynskikewin@gmail.com>
2023-03-01 18:49:07 +0100

    Added Data Annotations

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,9 +1,12 @@
-﻿namespace BulkyBookWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
 {
     public class Category
     {
-       
-        public int MyProperty { get; set; }
+        [Key]
+        public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
